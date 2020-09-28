b0a5cc23bcfe0c93f39242f0f964ba8a86625ccc
Aaron Smith <kuroroRBM@gmail.com>
2020-09-28 19:56:43 -0400

    added turn input

--- Assets/Scripts/LightCycleController.cs
+++ Assets/Scripts/LightCycleController.cs
@@ -9,13 +9,16 @@ namespace Tron
     [RequireComponent(typeof(Rigidbody))]
     public class LightCycleController : MonoBehaviour
     {
-        [SerializeField] private float speed = 100;
         public InputController controls;
 
-        private bool engineStarted = false;
+
         private Rigidbody rb;
 
+        [SerializeField] private float _maxVelocity = 100;
+        [SerializeField] private float _acceleration = 100;
 
+        private Vector3 _currentDirection = new Vector3();
+        private bool _engineStarted = false;
 
         // Start is called before the first frame update
         void Start()
@@ -25,29 +28,44 @@ namespace Tron
             controls = new InputController();
             controls.Enable();
             controls.Player.StartEngine.performed += StartEngine;
+            controls.Player.Turn.performed += TurnCycle;
+
         }
 
         // Update is called once per frame
         void Update()
         {
-            if (!engineStarted)
+            if (_engineStarted)
             {
-            }
-            else
-            {
-                rb.AddRelativeForce(Vector3.forward * speed, ForceMode.Force);
+                if (rb.velocity.sqrMagnitude < _maxVelocity*_maxVelocity)
+                {
+                    rb.velocity +=  _currentDirection * _acceleration * Time.deltaTime;
+                }
+
             }
         }
 
         public void StartEngine(InputAction.CallbackContext ctx)
         {
-            if (!engineStarted)
+            if (!_engineStarted)
             {
                 Debug.Log("Engine Starting!");
 
-                rb.AddRelativeForce(Vector3.forward * speed, ForceMode.Acceleration);
-                engineStarted = true;
+                _currentDirection = transform.forward;
+                transform.forward = _currentDirection;
+
+                _engineStarted = true;
             }
         }
+
+        public void TurnCycle(InputAction.CallbackContext ctx)
+        {
+            if (!_engineStarted) return;
+            var value = ctx.action.ReadValue<float>();
+            transform.RotateAround(transform.localToWorldMatrix*rb.centerOfMass, transform.up, 90);
+            rb.velocity = new Vector3();
+            _currentDirection = transform.forward;
+        }
+
     }
 }
